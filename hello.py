070c30573335c93ecaa65daa0461daf74c4b9ae0
Juho Kim <juho-kim@outlook.com>
2024-09-27 01:48:07 -0400

    Add email field and rename NameForm to UofTForm

--- hello.py
+++ hello.py
@@ -5,7 +5,7 @@ from flask_bootstrap import Bootstrap
 from flask import flash, Flask, redirect, render_template, session, url_for
 from flask_moment import Moment
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import EmailField, StringField, SubmitField
 from wtforms.validators import DataRequired
 
 app = Flask(__name__)
@@ -13,14 +13,18 @@ bootstrap = Bootstrap(app)
 moment = Moment(app)
 
 
-class NameForm(FlaskForm):
+class UofTForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+    email = EmailField(
+        'What is your UofT Email address?',
+        validators=[DataRequired()],
+    )
     submit = SubmitField('Submit')
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    form = NameForm()
+    form = UofTForm()
 
     if form.validate_on_submit():
         previous_name = session.get('name')
@@ -29,9 +33,20 @@ def index():
         if previous_name is not None and previous_name != session['name']:
             flash('Looks like you have changed your name!')
 
+        previous_email = session.get('email')
+        session['email'] = form.email.data
+
+        if previous_email is not None and previous_email != session['name']:
+            flash('Looks like you have changed your email!')
+
         return redirect(url_for('index'))
 
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template(
+        'index.html',
+        form=form,
+        name=session.get('name'),
+        email=session.get('email'),
+    )
 
 
 @app.route('/user/<name>')
