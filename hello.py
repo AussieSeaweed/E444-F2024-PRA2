fcb54d1fb03ffaf75746820a311324821e6f00cb
Juho Kim <juho-kim@outlook.com>
2024-09-27 01:46:54 -0400

    Implement Chapter 4 examples.
    Added:
    
    - Name changing form in the index page with flashing messages each time the user name changes.
    
    Changed:
    
    - Code refactoring.

--- hello.py
+++ hello.py
@@ -1,17 +1,37 @@
-from datetime import datetime
+from os import getenv
 
-from flask import Flask, render_template
+from dotenv import load_dotenv
 from flask_bootstrap import Bootstrap
+from flask import flash, Flask, redirect, render_template, session, url_for
 from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
 
-@app.route('/')
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    form = NameForm()
+
+    if form.validate_on_submit():
+        previous_name = session.get('name')
+        session['name'] = form.name.data
+
+        if previous_name is not None and previous_name != session['name']:
+            flash('Looks like you have changed your name!')
+
+        return redirect(url_for('index'))
+
+    return render_template('index.html', form=form, name=session.get('name'))
 
 
 @app.route('/user/<name>')
@@ -29,5 +49,13 @@ def internal_server_error(e):
     return render_template('500.html'), 500
 
 
-if __name__ == '__main__':
+def main():
+    load_dotenv()
+
+    app.config['SECRET_KEY'] = getenv('FLASK_SECRET_KEY')
+
     app.run(debug=True)
+
+
+if __name__ == '__main__':
+    main()
